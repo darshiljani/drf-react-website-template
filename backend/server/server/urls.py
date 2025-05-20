8ca5e5ec7a2605c918e9d900d8ed1061d9f59213
darshiljani <janidarshil92@gmail.com>
2025-05-20 18:23:45 +0530

    fix: make dev urls conditional

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -14,36 +14,51 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-from constants.permissions import InternalIPPermission
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Project API",
-        default_version="v1",
-        description="Description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="<your-gmail>@gmail.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.IsAdminUser, InternalIPPermission],
-)
+from constants.permissions import InternalIPPermission
+from django.contrib import admin
+from django.urls import path
+from rest_framework import permissions
+from utils.env import APP_ENV
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-     path(
-        "doc/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-    path("json/", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path("admin/", admin.site.urls),
 ]
+
+if APP_ENV != "prod":
+    from drf_yasg import openapi
+    from drf_yasg.views import get_schema_view
+
+    schema_view = get_schema_view(
+        openapi.Info(
+            title="Project API",
+            default_version="v1",
+            description="Description",
+            terms_of_service="https://www.google.com/policies/terms/",
+            contact=openapi.Contact(email="<your-gmail>@gmail.com"),
+            license=openapi.License(name="BSD License"),
+        ),
+        public=True,
+        permission_classes=[permissions.IsAdminUser, InternalIPPermission],
+    )
+
+    dev_urlpatterns = [
+        path(
+            "doc/",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        path(
+            "redoc/",
+            schema_view.with_ui("redoc", cache_timeout=0),
+            name="schema-redoc",
+        ),
+        path(
+            "json/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+        ),
+    ]
+
+    urlpatterns += dev_urlpatterns
 
 admin.sites.AdminSite.site_title = "Site Admin"
 admin.sites.AdminSite.site_header = "Site Dashboard"
